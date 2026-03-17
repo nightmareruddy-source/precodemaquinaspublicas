4705c466d46e42b1d6a1759e600ba78ff8a075af
nightmareruddy-source <nightmare.ruddy@gmail.com>
2026-03-17 11:17:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ app = FastAPI(title="Preço de Máquinas Públicas", version="0.1.0")
 
 @app.get("/")
 def home():
-    return {"status": "ok"}
+    return {
+        "projeto": "Preço de Máquinas Públicas",
+        "status": "online",
+        "dados_exemplo": [
+            {
+                "item": "Caminhão Basculante",
+                "municipio": "Ibiporã",
+                "valor": 480000
+            },
+            {
+                "item": "Retroescavadeira",
+                "municipio": "Londrina",
+                "valor": 320000
+            }
+        ]
+    }
 
 
 @app.get("/health")
