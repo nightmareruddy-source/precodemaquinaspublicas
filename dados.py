e98e95ac54f46aeb0702f10e3ef2f27b0e5c79ad
nightmareruddy-source <nightmare.ruddy@gmail.com>
2026-03-17 16:28:19 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,1 +1,17 @@
-
+dados_maquinas = [
+    {
+        "item": "Caminhão Basculante",
+        "municipio": "Ibiporã",
+        "valor": 480000
+    },
+    {
+        "item": "Retroescavadeira",
+        "municipio": "Londrina",
+        "valor": 320000
+    },
+    {
+        "item": "Motoniveladora",
+        "municipio": "Cambé",
+        "valor": 910000
+    }
+]
